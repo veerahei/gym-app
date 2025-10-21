112e213a81fa9aaa7079c667326836111de1d9a3
Veera <veera.heikkinen95@gmail.com>
2025-10-21 14:51:20 +0300

    Add fetch to show searched movements in a list

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import HomeScreen from './HomeScreen';
 import SearchScreen from './SearchScreen';
 import Test from './Test';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { PaperProvider } from 'react-native-paper';
 
 
 
@@ -11,36 +12,37 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
 
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-            if (route.name === 'Home') {
-              iconName = 'home'
-            } else if (route.name === 'Search') {
-              iconName = 'search-web'
-            } else if (route.name === 'Test') {
-              iconName = 'cards-heart'
-            }
+              if (route.name === 'Home') {
+                iconName = 'home'
+              } else if (route.name === 'Search') {
+                iconName = 'search-web'
+              } else if (route.name === 'Test') {
+                iconName = 'cards-heart'
+              }
 
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />
-          },
-          tabBarActiveTintColor: '#7E57C2',
-          tabBarShowLabel: false,
-          animation: 'shift' //For smooth transition between screens
-        })}
-      >
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />
+            },
+            tabBarActiveTintColor: '#7E57C2',
+            tabBarShowLabel: false,
+            animation: 'shift' //For smooth transition between screens
+          })}
+        >
 
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Search' component={SearchScreen} />
-        <Tab.Screen name='Test' component={Test}
-        />
-      </Tab.Navigator>
-    </NavigationContainer >
-
+          <Tab.Screen name='Home' component={HomeScreen} />
+          <Tab.Screen name='Search' component={SearchScreen} />
+          <Tab.Screen name='Test' component={Test}
+          />
+        </Tab.Navigator>
+      </NavigationContainer >
+    </PaperProvider>
   );
 }
 
--- SearchScreen.js
+++ SearchScreen.js
@@ -1,12 +1,56 @@
-import { View, StyleSheet } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { useState } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import { Button, Text, TextInput, Card } from "react-native-paper";
+
 
 
 export default function SearchScreen() {
 
+    const [input, setInput] = useState("");
+    const [exercises, setExercises] = useState([]);
+
+    const handleFetch = () => {
+        fetch(`https://www.exercisedb.dev/api/v1/exercises/search?q=${input}`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Error in fetch: ${response.statusText}`)
+                }
+                return response.json();
+            })
+            .then(data => setExercises(data.data))
+            .catch(error => console.log(error))
+
+        setInput("");
+    }
+
+    console.log(exercises);
+
     return (
-        <View style={styles.container}>
-            <Text variant="displaySmall">This is the search screen</Text>
+        <View style={styles.container} >
+            <TextInput style={styles.input}
+                label="Search exercises"
+                value={input}
+                onChangeText={text => setInput(text)}
+            />
+            <Button
+                mode="contained"
+                onPress={handleFetch}
+            >
+                Search
+            </Button>
+            <FlatList
+                style={{ width: '90%' }}
+                data={exercises}
+                renderItem={({ item }) =>
+                    <Card>
+                        <Card.Cover source={{ uri: item.gifUrl }} />
+                        <Card.Title title={item.name} />
+                        <Card.Content>
+                            <Text>{item.instructions}</Text>
+                        </Card.Content>
+                    </Card>
+                }
+            />
         </View>
     )
 }
@@ -16,6 +60,8 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
     },
+    input: {
+        width: '90%',
+    }
 });
