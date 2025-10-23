33bcea1e2b7e4a8ebb409a7515e5a327ad00425f
Veera <veera.heikkinen95@gmail.com>
2025-10-23 18:25:18 +0300

    Create and configure Google realtime database

--- Test.js
+++ Test.js
@@ -1,10 +1,32 @@
-import { Text } from "react-native-paper"
+import { Text, TextInput, Button } from "react-native-paper"
 import { View } from "react-native"
+import { getDatabase, push, ref, } from "firebase/database";
+import { app } from './firebaseConfig';
+import { useState } from "react";
 
 export default function Test() {
+    const [workoutPlan, setWorkoutPlan] = useState("");
+
+    const db = getDatabase(app);
+
+    function addWorkoutPlan() {
+        console.log("in the function")
+        push(ref(db, 'plans/'), workoutPlan)
+    }
+
+
     return (
         <View>
-            <Text>Test page</Text>
+            <TextInput
+                label="Name of the workout plan"
+                value={workoutPlan}
+                onChangeText={input => setWorkoutPlan(input)}
+            />
+            <Button
+                onPress={addWorkoutPlan}
+            >
+                Add new workout plan
+            </Button>
         </View>
     )
 }
---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBQf-ShhRdIn3HDY4GcNLznovsFE7IT8as",
+    authDomain: "gymapp-9790e.firebaseapp.com",
+    databaseURL: "https://gymapp-9790e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gymapp-9790e",
+    storageBucket: "gymapp-9790e.firebasestorage.app",
+    messagingSenderId: "1001063481083",
+    appId: "1:1001063481083:web:1c1ae65bb7db3bfa927a5e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
