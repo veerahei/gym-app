d7d6f7feab6846db2bf3080fb190a9c023cda7c4
Veera <veera.heikkinen95@gmail.com>
2025-11-21 13:40:19 +0200

    Modify Search screen's UI to show user's latest search word. Add button to allow user to clear latest search.

--- SearchScreen.js
+++ SearchScreen.js
@@ -6,6 +6,7 @@ export default function SearchScreen() {
 
     const [input, setInput] = useState("");
     const [exercises, setExercises] = useState([]);
+    const [latestSearch, setLatestSearch] = useState("");
 
     const handleFetch = () => {
         fetch(`https://www.exercisedb.dev/api/v1/exercises/search?q=${input}`)
@@ -18,25 +19,43 @@ export default function SearchScreen() {
             .then(data => setExercises(data.data))
             .catch(error => console.log(error))
 
+        setLatestSearch(input)
         setInput("");
+
+    }
+
+
+    const handleClearSearch = () => {
+        setExercises([]);
+        setLatestSearch("");
     }
 
     console.log(exercises);
 
     return (
+        
         <View style={styles.container} >
-            <Text style={{ marginTop: 100 }}>Search inspiration for your next workout</Text>
+            <Text style={{ paddingTop: 40, paddingBottom: 20 }} variant="titleMedium">Search inspiration for your next workout</Text>
             <Searchbar style={styles.input}
-                placeholder=""
+                placeholder="e.g. targeted area, equipment, movement"
                 value={input}
                 onChangeText={text => setInput(text)}
             />
-            <Button
-                mode="contained"
-                onPress={handleFetch}
-            >
-                Search
-            </Button>
+            <View style={{flexDirection: "row", gap: 10}}>
+                <Button
+                    
+                    onPress={handleFetch}
+                >
+                    Search
+                </Button>
+                <Button
+                    onPress={handleClearSearch}
+                >Clear
+                </Button>
+            </View>
+            {latestSearch != "" &&
+                <Text variant="labelSmall">Your search for '{latestSearch}'</Text>}
+
             <FlatList
                 style={{
                     marginTop: 10, width: '100%',
@@ -46,13 +65,12 @@ export default function SearchScreen() {
                 renderItem={({ item }) =>
                     <Card style={{ marginBottom: 20, flex: 1, }}>
 
-                        <Card.Title title={item.name} />
+                        <Card.Title title={item.name.charAt(0).toUpperCase() + item.name.slice(1)} />
                         <View style={{ backgroundColor: "white", marginHorizontal: 20, alignItems: "center", justifyContent: 'center', marginBottom: 20, borderRadius: 10, }}>
                             <Image
                                 source={{ uri: item.gifUrl }}
                                 style={{ width: "100%", height: 200, resizeMode: "contain", }}
                             >
-
                             </Image>
                         </View>
 
